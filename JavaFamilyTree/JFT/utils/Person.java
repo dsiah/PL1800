bd73333c4eab46a0502c7c61283908cb4898999f
David Siah <dsiah@villanova.edu>
2015-09-07 15:13:23 -0400

    git example

--- JavaFamilyTree/JFT/utils/Person.java
+++ JavaFamilyTree/JFT/utils/Person.java
@@ -3,7 +3,7 @@ package JFT.utils;
 import java.util.ArrayList;
 
 public class Person {
-	boolean isAncestor; // Adam or Eve
+	boolean isTopLevel; // Adam or Eve
 	String[] parents = new String[2]; // Max Two parents
 	ArrayList<String> ancestors = new ArrayList<String>();
 	ArrayList<String> siblings  = new ArrayList<String>();
