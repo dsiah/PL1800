a0a3daccc50c974ee51e1f7886cc249ab9414717
David Siah <dsiah@villanova.edu>
2015-09-07 15:54:20 -0400

    testing hash

--- JavaFamilyTree/FamilyTree.java
+++ JavaFamilyTree/FamilyTree.java
@@ -3,7 +3,15 @@ import JFT.utils.*;
 public class FamilyTree {
 	public static void main(String[] args) {
 		System.out.println("HELLO");
-		Parser p = new Parser();
-		p.scanLine();
+		
+		//Parser p = new Parser();
+		//p.scanLine();
+		
+		Person sid = new Person("Sid");
+		PeopleHash ph = new PeopleHash();
+		ph.addPerson("Sid", sid);
+		
+		System.out.println(ph.lookupPerson("Sid"));
+		System.out.println(ph.lookupPerson("Bob"));
 	}
 }
--- JavaFamilyTree/JFT/utils/Person.java
+++ JavaFamilyTree/JFT/utils/Person.java
@@ -3,8 +3,18 @@ package JFT.utils;
 import java.util.ArrayList;
 
 public class Person {
+	String name;
 	boolean isTopLevel; // Adam or Eve
 	String[] parents = new String[2]; // Max Two parents
 	ArrayList<String> ancestors = new ArrayList<String>();
 	ArrayList<String> children  = new ArrayList<String>();
+	
+	public Person(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
 }
