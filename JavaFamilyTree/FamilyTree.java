63fdaef82c491929a4ae92f0087d25524b6682b5
David Siah <dsiah@villanova.edu>
2015-09-07 18:18:20 -0400

    attempting to remove binaries and class files from git repo

--- JavaFamilyTree/FamilyTree.java
+++ JavaFamilyTree/FamilyTree.java
@@ -2,12 +2,8 @@ import JFT.utils.*;
 
 public class FamilyTree {
 	public static void main(String[] args) {
-		PeopleHash ph = new PeopleHash();
+		PeopleHash ph = new PeopleHash(); // keep this global
 		Parser p = new Parser(ph);
-		p.parse(); // will keep parsing until you enter Ctrl-D in terminal/console 
-		
-		//Person sid = new Person("Sid");
-		//ph.addPerson("Sid", sid);
-		//System.out.println(ph.lookupPerson("Sid"));
+		p.parse(); // will keep parsing until you enter Ctrl-D in terminal/console 		
 	}
 }
