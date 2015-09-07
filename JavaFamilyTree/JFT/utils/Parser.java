c1ec5e7967035d8c79bbae234ef31f26146b3f96
David Siah <dsiah@villanova.edu>
2015-09-07 16:17:18 -0400

    parsing and person methods declared

--- JavaFamilyTree/JFT/utils/Parser.java
+++ JavaFamilyTree/JFT/utils/Parser.java
@@ -14,7 +14,6 @@ public class Parser {
 		switch (in) {
 			case ("E"):
 				// Call event method
-				event();
 				System.out.println("Event seen!");
 				break;
 
@@ -30,24 +29,5 @@ public class Parser {
 				// Call get listRelatives
 				break;
 		}
-	}
-
-	// All the private methods to be called from the cases
-	void event() {
-		System.out.println("Calling Event!");
-		return;
-	}
-
-	String closestRelation(String name1, String name2) {
-		return "RELATIONSHIP GOES HERE"; // or unrelated
-	}
-
-	boolean isRelatedBy(String name1, String relation, String name2) {
-		return false; // Print Yes or No
-	}
-
-	String listRelatives(String name1) {
-		// Must be sorted alphabetically & listed
-		return "RELATIVES GO HERE";
 	}
 }
--- JavaFamilyTree/JFT/utils/Person.java
+++ JavaFamilyTree/JFT/utils/Person.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class Person {
 	String name;
-	boolean isTopLevel; // Adam or Eve
+	boolean isTopLevel; // Adam or Eve Generation
 	String[] parents = new String[2]; // Max Two parents
-	ArrayList<String> ancestors = new ArrayList<String>();
-	ArrayList<String> children  = new ArrayList<String>();
+	ArrayList<String> spouses  = new ArrayList<String>();
+	ArrayList<String> children = new ArrayList<String>();
 	
 	public Person(String name) {
 		this.name = name;
